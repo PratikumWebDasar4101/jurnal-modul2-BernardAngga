7f25400914b90622ad8c76bf79400ee15871f8f2
Bernard Angga Sihotang <42725315+BernardAngga@users.noreply.github.com>
2018-09-09 19:56:13 -0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -21,4 +21,27 @@
 $inputkanja = $_GET["inputkan"];
 $i = 0;
 rekursif($inputkanja);
+
+for ($i=1;$i<=$inputkanja; $++){
+	if($i % 2 == 0){
+		$genap[]=$i;
+	}else{
+$ganjil[]=$i;
+}
+}
+
+echo "genap = ";
+foreach ($ganjil as $bil_genap) {
+	echo $bil_genap . ',';
+	# code...
+}
+echo "<br>";
+echo "ganjil = ";
+foreach ($ganjil as $bil_ganjil) {
+	echo $bil_ganjil . ',';
+	# code...
+}
+
+echo "<br><br>Jumlah deret Genap = ".count($genap) . "<br>";
+echo "Jumlah deret ganjil = " . count($ganjil);
 ?>
