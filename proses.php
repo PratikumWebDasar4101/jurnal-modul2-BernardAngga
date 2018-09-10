b5bc0f7a5d7217f81739a7bffd1ed87130c7d56c
Bernard Angga Sihotang <42725315+BernardAngga@users.noreply.github.com>
2018-09-09 19:47:55 -0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,24 @@
-<?php 
-//Tuliskan kode anda di bawah ini
+<?php
+	function rekursif($rekf){
+		global $i;
 
+		rekursif2($i,$rekf);
+		echo "<br>";
+		if(--$rekf>$i){
+			rekursif($rekf);
+
+	}
+		}
+		function rekursif2($k,$rekf){
+			global $i;
+
+		if($k>$i -$rekf){
+	echo "*";
+	rekursif2($k-1,$rekf);
+	}
+}
+
+$inputkanja = $_GET["inputkan"];
+$i = 0;
+rekursif($inputkanja);
 ?>
